4c59093d6235b6edd358343ca4145f1edec26458
Sampo Jaakkola <sampo.jaakkola@myy.haaga-helia.fi>
2023-11-12 21:36:40 +0200

    Added a placeholder menu and the About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Products from './Components/Products'
+import React, { useState } from 'react'
+import Tabs from '@mui/material/Tabs'
+import Tab from '@mui/material/Tab'
+import Main from './components/Main'
+import Products from './components/Products'
+import About from './components/About'
 
-function App() {
- 
+
+export default function App() {
+
+  const [renderedPage, setRenderedPage] = useState("main")
+
+  const navigate = (event, renderedPage) => {
+    setRenderedPage(renderedPage)
+  }
 
   return (
-    <Products>
-    </Products>
+    <div className="App">
+      <Tabs value={renderedPage} onChange={navigate}>
+        <Tab value="main" label="Main" />
+        <Tab value="products" label="Products" />
+        <Tab value="about" label="About" />
+      </Tabs>
+      {renderedPage === "main" && <Main />}
+      {renderedPage === "products" && <Products />}
+      {renderedPage === "about" && <About />}
+    </div>
 
-   
   )
 }
-
-export default App
